7e8a19c9934424fc9576ae08aa9a13bb72771ec1
hojoooon <62469572+hojoooon@users.noreply.github.com>
2020-08-20 14:36:55 +0900

    Add files via upload

---
+++ chatbot_2.js
@@ -0,0 +1,8 @@
+const app = require("./MyNodeApp/app");
+
+app.post('/', function(req, res){
+    console.log("MESSAGE RESPONSE");
+    console.log(JSON.stringify(req.body, null, "  "));
+
+    res.send('OK!');
+});
